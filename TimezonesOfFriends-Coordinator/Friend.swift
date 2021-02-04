c0270e63d36b8eb89d2a244931d6f0b5e8ba53a7
Justyna <kowalkowska.just@gmail.com>
2021-02-05 00:44:40 +0100

    Creating Friends model file.

---
+++ TimezonesOfFriends-Coordinator/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  TimezonesOfFriends-Coordinator
+//
+//  Created by Justyna Kowalkowska on 05/02/2021.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var name = "New friend"
+    var timeZone = TimeZone.current
+}
---
+++ TimezonesOfFriends-Coordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimezonesOfFriends-Coordinator
+//
+//  Created by Justyna Kowalkowska on 05/02/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    //MARK: - Properties
+    
+    var friends = [Friend]()
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
